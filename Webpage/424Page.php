76dadc6ea3382eb8959f8e26df31f90a60a4281e
Kelly C Hattler <khattle@titan4.cs.clemson.edu>
2017-04-14 16:33:55 -0400

    Webpage

---
+++ Webpage/424Page.php
@@ -0,0 +1,28 @@
+
+<?php
+//DB Connect
+$connection = mysqli_connect('mysql1.cs.clemson.edu','khattle','420noscopeblazeit');
+mysqli_select_db($connection, 'ssadatabase');
+
+$query = "SELECT * FROM Iterations";
+$result = mysqli_query($connection, $query);
+
+
+// Printing results in HTML
+echo "<h1>Database Contents</h1>";
+echo "<table>\n";
+echo "\t<tr>\n \t\t<td>IP</td>\t\t<td>Time</td>\t\t<td>RTTMin</td>\t\t<td>RTTMax</td>\t\t<td>RTTAverage</td>\n";
+while ($line = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+   echo "\t<tr>\n";
+   foreach ($line as $col_value) {
+      echo "\t\t<td>$col_value</td>\n";
+   }
+   echo "\t</tr>\n";
+}
+
+echo "</table>\n";
+
+mysqli_free_result($result);
+
+mysqli_close($connection);
+?>
