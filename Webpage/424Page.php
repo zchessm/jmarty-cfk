ffd3f6d31ab3a739a8a053ebf45a500f8722c6f3
Kelly C Hattler <khattle@koala22.cs.clemson.edu>
2017-04-30 17:35:26 -0400

    stuff

--- Webpage/424Page.php
+++ Webpage/424Page.php
@@ -6,12 +6,12 @@ mysqli_select_db($connection, 'ssadatabase');
 $salt = 'b0t@t0';
 
 $query = "SELECT *,
-AES_DECRYPT('IP', '".$salt."') AS IP,
-AES_DECRYPT('Time', '".$salt."') AS Time,
-AES_DECRYPT('RTTMin', '".$salt."') AS RTTMin,
-AES_DECRYPT('RTTMax', '".$salt."') AS RTTMax,
-AES_DECRYPT('RTTAverage', '".$salt."') AS RTTAverage,
-AES_DECRYPT('ID', '".$salt."') AS ID
+CAST(AES_DECRYPT('IP', 'b0t@t0')AS VARCHAR(10000)) AS IP,
+CAST(AES_DECRYPT('Time', 'b0t@t0')AS VARCHAR(10000)) AS Time,
+CAST(AES_DECRYPT('RTTMin', 'b0t@t0')AS VARCHAR(10000)) AS RTTMin,
+CAST(AES_DECRYPT('RTTMax', 'b0t@t0')AS VARCHAR(10000)) AS RTTMax,
+CAST(AES_DECRYPT('RTTAverage', 'b0t@t0')AS VARCHAR(10000)) AS RTTAverage,
+CAST(AES_DECRYPT('ID', 'b0t@t0') AS ID
 FROM Iterations";
 $result = mysqli_query($connection, $query);
 
@@ -27,7 +27,7 @@ while ($line = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
    echo "\t\t<td>" . $line['Time'] . "</td>\n";
 	echo "\t\t<td>" . $line['RTTMin'] . "</td>\n";
 	echo "\t\t<td>" . $line['RTTMax'] . "</td>\n";
-	echo "\t\t<td>" . $line['RTTAvg'] . "</td>\n";
+	echo "\t\t<td>" . $line['RTTAverage'] . "</td>\n";
 	echo "\t\t<td>" . $line['ID'] . "</td>\n";
    echo "\t</tr>\n";
 }
