49b2dcc0412ca69e9f0706fac57b1dc6dad653f9
Kelly C Hattler <khattle@koala22.cs.clemson.edu>
2017-04-30 16:45:50 -0400

    424Page

--- Webpage/424Page.php
+++ Webpage/424Page.php
@@ -3,8 +3,16 @@
 //DB Connect
 $connection = mysqli_connect('mysql1.cs.clemson.edu','khattle','420noscopeblazeit');
 mysqli_select_db($connection, 'ssadatabase');
+$salt = 'b0t@t0';
 
-$query = "SELECT * FROM Iterations";
+$query = "SELECT *, 
+AES_DECRYPT(IP, '.$salt.') AS IP_clear,
+AES_DECRYPT(Time, '.$salt.') AS Time_clear,
+AES_DECRYPT(RTTMin, '.$salt.') AS RTTMin_clear,
+AES_DECRYPT(RTTMax, '.$salt.') AS RTTMax_clear,
+AES_DECRYPT(RTTAverage, '.$salt.') AS RTTAvg_clear,
+AES_DECRYPT(ID, '.$salt.') AS ID_clear
+FROM Iterations";
 $result = mysqli_query($connection, $query);
 
 
@@ -12,11 +20,15 @@ $result = mysqli_query($connection, $query);
 echo "<h1>Database Contents</h1>";
 echo "<table>\n";
 echo "\t<tr>\n \t\t<td>IP</td>\t\t<td>Time</td>\t\t<td>RTTMin</td>\t\t<td>RTTMax</td>\t\t<td>RTTAverage</td>\t\t<td>ID</td>\n";
+
 while ($line = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
    echo "\t<tr>\n";
-   foreach ($line as $col_value) {
-      echo "\t\t<td>$col_value</td>\n";
-   }
+	echo "\t\t<td>" . $line['IP_clear'] . "</td>\n";
+   echo "\t\t<td>" . $line['Time_clear'] . "</td>\n";
+	echo "\t\t<td>" . $line['RTTMin_clear'] . "</td>\n";
+	echo "\t\t<td>" . $line['RTTMax_clear'] . "</td>\n";
+	echo "\t\t<td>" . $line['RTTAvg_clear'] . "</td>\n";
+	echo "\t\t<td>" . $line['ID_clear'] . "</td>\n";
    echo "\t</tr>\n";
 }
 
