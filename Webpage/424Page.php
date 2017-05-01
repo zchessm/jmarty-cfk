fd8aa9770ff8d881bb6dcdaf0ea8a896505168cc
Kelly C Hattler <khattle@koala6.cs.clemson.edu>
2017-05-01 17:37:09 -0400

    Login

--- Webpage/424Page.php
+++ Webpage/424Page.php
@@ -1,4 +1,7 @@
 <?php
+	session_start();
+	include_once "function.php";
+
 
 
 /*if(!isset ($_SESSION['username'])) {
@@ -14,7 +17,7 @@ $result = mysql_query($query);
 echo "<h1>Database Contents</h1>";
 echo "<table>\n";
 echo "\t<tr>\n \t\t<td>IP</td>\t\t<td>Time</td>\t\t<td>RTTMin</td>\t\t<td>RTTMax</td>\t\t<td>RTTAverage</td>\t\t<td>ID</td>\n";
-while ($line = mysql_fetch_array($result, MYSQLI_ASSOC)) {
+while ($line = mysql_fetch_row($result)) {
    echo "\t<tr>\n";
    foreach ($line as $col_value) {
       echo "\t\t<td>$col_value</td>\n";
